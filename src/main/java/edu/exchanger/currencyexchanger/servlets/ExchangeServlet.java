10b5b028dc348e9c35ebd3bba9897d1695aebe1d
Dmytro <dmytro.kupets@gmail.com>
2024-01-19 19:25:14 +0200

    add GET in ExchangeServlet.java

---
+++ src/main/java/edu/exchanger/currencyexchanger/servlets/ExchangeServlet.java
@@ -0,0 +1,60 @@
+package edu.exchanger.currencyexchanger.servlets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.exchanger.currencyexchanger.ExchangeService;
+import edu.exchanger.currencyexchanger.models.Currency;
+import edu.exchanger.currencyexchanger.repositories.CurrencyRepository;
+import edu.exchanger.currencyexchanger.repositories.ExchangeRatesRepository;
+import edu.exchanger.currencyexchanger.util.Util;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@WebServlet(value = "/exchange")
+public class ExchangeServlet extends HttpServlet {
+    CurrencyRepository currencyRepository;
+    ExchangeRatesRepository exchangeRatesRepository;
+
+    @Override
+    public void init() throws ServletException {
+        currencyRepository = new CurrencyRepository();
+        exchangeRatesRepository = new ExchangeRatesRepository();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String from = req.getParameter("from");
+        String to = req.getParameter("to");
+        String amount = req.getParameter("amount");
+
+        if (Util.isNotValidExchangeArgs(from, to, amount) || !Util.isStringDouble(amount)) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Неправильно введен запрос. Пример: /exchange?from=USD&to=RUB&amount=10");
+            return;
+        }
+
+        if (!isCurrenciesValid(from, to)) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Указана не существующая валюта. Пример: /exchange?from=USD&to=RUB&amount=10");
+            return;
+        }
+
+        ExchangeService exchangeService = ExchangeService.builder()
+                .exchangeRate(exchangeRatesRepository.findByCodes(from,to).get())
+                .amount(BigDecimal.valueOf(Double.parseDouble(amount)))
+                .build();
+
+        new ObjectMapper().writeValue(resp.getWriter(),exchangeService.getExchange());
+
+    }
+    private boolean isCurrenciesValid(String from, String to) {
+        Optional<Currency> fromCurrency = currencyRepository.findByCode(from);
+        Optional<Currency> toCurrency = currencyRepository.findByCode(to);
+
+        return (fromCurrency.isPresent() && toCurrency.isPresent());
+    }
+}
